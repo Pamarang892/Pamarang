99d4be9d78568398442e608b22b2753f35f71cfa
Pamarang892 <136693136+Pamarang892@users.noreply.github.com>
2023-06-15 16:57:23 +0800

    Add files via upload

---
+++ Pamarang/Form2.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form2
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Pamarang
+        If My.Settings.SerialKey = TextBox1.Text Then
+            My.Settings.AppStat = "Full Version"
+            My.Settings.Save()
+            Me.Close()
+        Else
+            MsgBox("Invalid Serial Key")
+        End If
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
